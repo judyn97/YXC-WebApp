eeab02de075d6b76f6c8a5c43a6a0aee3998e579
jalal <jalaludin5011@gmail.com>
2024-01-06 14:09:16 +0800

    Add new functions
    - Add new function for user to input the product IP address
    - Add new function to check for model name of the connected device
    - Delete last commit test to fix https error

--- script.js
+++ script.js
@@ -7,6 +7,37 @@ let inputList = [];
 let inputListID = [];
 
 const inputSelect = document.querySelector("#input-select");
+const ipAddressInput = document.querySelector("#ip-input");
+const ipContainer = document.querySelector("#ip-container");
+const inputContainer = document.querySelector("#input-container");
+const modelConnected = document.querySelector("#model-id");
+const powerStatus = document.querySelector("#power-id");
+let deviceIPAddress = '';
+
+async function checkDeviceIP(){
+    try{
+        let ipAddress = ipAddressInput.value;
+        deviceIPAddress = ipAddress;
+        console.log(ipAddress);
+        const response = await fetch(`http://${ipAddress}/YamahaExtendedControl/v1/system/getDeviceInfo`);
+        if(!response.ok){
+            throw new Error("Could not fetch");
+        }
+        const data = await response.json();
+
+        if(data.response_code === 0)
+        {
+            ipContainer.style.display = "none";
+            inputContainer.style.display = "flex";
+            modelConnected.innerText = `Model connected to: ${data.model_name}`;
+            getStatus();
+        }
+    }
+    catch(error){
+        console.error(error);
+        modelConnected.innerText = `${error}`;
+    }
+}
 
 async function getFeatures(){
     try{
@@ -34,12 +65,27 @@ async function changeInput(){
     try{
         let inputSelected = inputSelect.value;
         if(inputSelected == ""){
-            alert("Please Add input first !")
+            alert("Please Get input first !")
         }
         else{
             const response = await fetch(`http://192.168.0.145/YamahaExtendedControl/v1/main/setInput?input=${inputSelected}`);
             const data = await response.json()
+            console.log("change",data.response_code)
         }
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+async function getStatus(){
+    try{
+        console.log("122",deviceIPAddress);
+        const response = await fetch(`http://${deviceIPAddress}/YamahaExtendedControl/v1/main/getStatus`);
+        const data = await response.json();
+
+        powerStatus.innerText = `Device power status: ${data.power}`
+
     }
     catch(error){
         console.error(error);
